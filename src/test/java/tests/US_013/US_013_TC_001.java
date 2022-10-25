dc3e2c8f428e41341911a89ad02dbf8f464fd59e
afra94 <kolotafra@gmail.com>
2022-10-25 10:32:56 +0300

    25.10.2022

--- src/test/java/tests/US_013/US_013_TC_001.java
+++ src/test/java/tests/US_013/US_013_TC_001.java
@@ -11,23 +11,28 @@ import pages.APearlyMarketPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
 import java.util.List;
 
-public class US_013_TC_001 {
+public class US_013_TC_001 extends TestBaseRapor {
     APearlyMarketPage pm;
     Actions actions;
 
     @Test
+
     public void cuopponPersentage() throws InterruptedException {
+        extentTest = extentReports.createTest("couppon testi", "olusturulan couppon'un kaydi");
         pm = new APearlyMarketPage();
         Driver.getDriver().get(ConfigReader.getProperty("pearlyUrl"));
         //1. vendor basarili bir sekilde sign in olur
         Thread.sleep(2000);
         ReusableMethods.prMrktlogIn();
+        extentTest.info("log in yapildi");
         // 2.MY ACCOUNT secenegine tiklar
         // 3.store manager bolumune tiklar
         ReusableMethods.prMrktstoreManager();
+        extentTest.info("store manager'a tiklandi");
         //4.coupons bolumunden "add new" secenegini secer
         actions = new Actions(Driver.getDriver());
         WebElement couppon = pm.acoupons;
@@ -35,29 +40,38 @@ public class US_013_TC_001 {
         actions.sendKeys(Keys.ARROW_DOWN).
                 sendKeys(Keys.ARROW_DOWN).perform();
         actions.moveToElement(couppon).click(addNew).perform();
+        extentTest.info("couppon olusturmak icin add new secenegine tiklandi");
         // 5.code butonuna  sayisal deger ve harf yazar
         pm.acode.sendKeys(ConfigReader.getProperty("coupponName1"));
+        extentTest.info("couppon code'u girildi");
         // 6.kuponu turune gore aciklar
         pm.adescription.sendKeys(ConfigReader.getProperty("description1"));
+        extentTest.info("couppon icin aciklama yapildi");
         // 7.Discount Type menusundeki seceneklerden "percentage discount'u" secer
         WebElement discountType = pm.adiscountType;
         Select options = new Select(discountType);
         options.selectByIndex(0);
+        extentTest.info("indirim turu secildi");
         //8.Coupon Amount butonuna 1-100 arasinda bir rakam girer
         pm.aamount.clear();
         pm.aamount.sendKeys(ConfigReader.getProperty("amount1"));
+        extentTest.info("indirimin degeri girildi");
         // 9.Coupon expiry date bolumune guncel tarih veya sonrasi bir deger  girer
-        pm.adate.click();
-        pm.adayNumber.click();
+        ReusableMethods.click(pm.adate);
+        pm.adate.sendKeys("2022-10-30");
+        extentTest.info("son gecerlilij tarihi belirlendi");
         //10.Kupon ücretsiz gönderim sağlıyorsa Free shipping kutucugunu isaretller
         WebElement free =pm.afreeShipping;
         ReusableMethods.click(free);
+        extentTest.info("free shippin kutucugu isaretlendi");
         // 11.Kuponu mağaza sayfasında göstermek istiyorsa "Show on store" kutucugunu secebilmeli
         WebElement show=pm.ashow;
         ReusableMethods.click(show);
+        extentTest.info("anasayfada gosterme kutucugu isaretlendi");
         // 12.submit'e basar
         WebElement submit=pm.asubmit;
         ReusableMethods.click(submit);
+        extentTest.info("couppon kaydedildi");
 
 
         //13.kazan25 isimli kuponun kaydedildigini dogrular
@@ -66,5 +80,6 @@ public class US_013_TC_001 {
         String actualAmount=codeCells.get(0).getText();
         String expectedAmount="kazan25";
         Assert.assertEquals(actualAmount,expectedAmount);
+        extentTest.pass("kaydedilen couppon listede gorundu");
     }
 }
