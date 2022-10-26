9931b72a71f9c59c5bee153f99e87cb423e97116
Cuneyt Erdemir <emrecuneyt02@gmail.com>
2022-10-26 03:08:05 +0300

    26.10.2022

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.C4PearlyMarketPage;
 import pages.EPearlyMarketPage;
 import pages.PearlyMarketPage;
 
